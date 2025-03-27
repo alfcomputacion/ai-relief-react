e0e406c99978b962894d4938db34ec9287cff4cb
Jose Rosas <alfcomputacion@gmail.com>
2025-03-26 17:48:11 -0700

    changed url

--- ai-relief-r/src/components/AgentNextSteps.jsx
+++ ai-relief-r/src/components/AgentNextSteps.jsx
@@ -13,7 +13,7 @@ useEffect(()=>{
       console.log('trying AI-API')
       
       // try {
-        const response = await fetch ('http://127.0.0.1:8000/api/airelief/',{
+        const response = await fetch ('http://127.0.0.1:8000/api/airelief/text/',{
           method: "POST",
           headers: {"Content-Type": "application/json"},
           body: JSON.stringify({username:"user"})
