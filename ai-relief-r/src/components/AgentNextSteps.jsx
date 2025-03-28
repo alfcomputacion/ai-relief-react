1dcb7bbc9c153b4ac3383a237b489ffe01b64185
Jose Rosas <alfcomputacion@gmail.com>
2025-03-28 12:46:27 -0700

    working and it sends title of pdf

--- ai-relief-r/src/components/AgentNextSteps.jsx
+++ ai-relief-r/src/components/AgentNextSteps.jsx
@@ -4,10 +4,15 @@ import AiReliefContext from '../context/AiReliefProvider'
 
 export default function AgentNextSteps() {
 const [response, setResponse] = useState(false);
+const [fileName, setFileName] = useState('')
 const {aiResponse, setAiResponse} = useContext(AiReliefContext);
+
 useEffect(()=>{
   console.log("response changed")
 },[response])
+const handleInput = (e)=>{
+  setFileName(e.target.value);
+}
     // handles assesment api call
     const handleAssesment = async (e) =>{
       console.log('trying AI-API')
@@ -16,7 +21,7 @@ useEffect(()=>{
         const response = await fetch ('http://127.0.0.1:8000/api/airelief/text/',{
           method: "POST",
           headers: {"Content-Type": "application/json"},
-          body: JSON.stringify({username:"user"})
+          body: JSON.stringify({username:"user", fileName})
           })
           const data =  await response.json();
           if (data){
@@ -32,9 +37,11 @@ useEffect(()=>{
   return (
     <>
      <p>Hola, Hello, Bonjour, Hallo, Ciao, Olá, こんにちは</p> 
+     <input type="text" value={fileName} onChange={handleInput} placeholder='Name of the file'/>
       <button onClick={() => handleAssesment()  } className='btn btn-info m-1'>Analize</button>
       {response?<div style={{whiteSpace: 'pre-wrap'}} className='text-start w-75 h-75 m-auto pt-4'>
         {aiResponse}
+        {fileName}
       </div>:<div>...</div>}
     </>
   )
