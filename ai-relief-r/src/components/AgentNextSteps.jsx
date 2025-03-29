30cfd2513da1bd3af6dcd6626814cef5c4ba96b1
Jose Rosas <alfcomputacion@gmail.com>
2025-03-28 21:33:39 -0700

    shoud be updated

--- ai-relief-r/src/components/AgentNextSteps.jsx
+++ ai-relief-r/src/components/AgentNextSteps.jsx
@@ -4,14 +4,24 @@ import AiReliefContext from '../context/AiReliefProvider'
 
 export default function AgentNextSteps() {
 const [response, setResponse] = useState(false);
-const [fileName, setFileName] = useState('')
+const [formData, setFormData] = useState({
+  fileName: '',
+  instructions: 'Resume'
+  
+})
+
 const {aiResponse, setAiResponse} = useContext(AiReliefContext);
 
 useEffect(()=>{
   console.log("response changed")
 },[response])
+
 const handleInput = (e)=>{
-  setFileName(e.target.value);
+  const {name, value} = e.target;
+  setFormData((prevData)=>({
+    ...prevData,
+    [name]: value,
+  }));
 }
     // handles assesment api call
     const handleAssesment = async (e) =>{
@@ -21,7 +31,7 @@ const handleInput = (e)=>{
         const response = await fetch ('http://127.0.0.1:8000/api/airelief/text/',{
           method: "POST",
           headers: {"Content-Type": "application/json"},
-          body: JSON.stringify({username:"user", fileName})
+          body: JSON.stringify(formData)
           })
           const data =  await response.json();
           if (data){
@@ -37,11 +47,22 @@ const handleInput = (e)=>{
   return (
     <>
      <p>Hola, Hello, Bonjour, Hallo, Ciao, Olá, こんにちは</p> 
-     <input type="text" value={fileName} onChange={handleInput} placeholder='Name of the file'/>
+     
+     <input type="text"
+            name="fileName"
+            value={formData.fileName}
+            onChange={handleInput}
+            placeholder='title of file' />
+     <input 
+        type="text" 
+        name="instructions" 
+        value={formData.instructions} 
+        onChange={handleInput} 
+        placeholder='Instructions for AI'/>
+
       <button onClick={() => handleAssesment()  } className='btn btn-info m-1'>Analize</button>
       {response?<div style={{whiteSpace: 'pre-wrap'}} className='text-start w-75 h-75 m-auto pt-4'>
         {aiResponse}
-        {fileName}
       </div>:<div>...</div>}
     </>
   )
